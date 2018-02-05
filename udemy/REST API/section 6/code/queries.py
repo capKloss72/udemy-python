5b67a29b84274488e90638e4160f5273d2246c63
capkloss72 <Nemesis72>
2018-02-05 12:43:36 +0800

    - added following initial refactor of REST

---
+++ udemy/REST API/section 6/code/queries.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+query = "INSERT INTO items VALUES(NULL, 'pin', 12.25)"
+#query = "SELECT * FROM users"
+#query = "SELECT * FROM items"
+#query = "DELETE FROM items WHERE name = 'plate'"
+cursor.execute(query)
+connection.commit()
+
+query = "SELECT * FROM items"
+for i in cursor.execute(query):
+    print(i)
